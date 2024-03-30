8ecdc0073f7a75abffadf830f7c684b84f9ffd0b
Farhan-Petkar <165374171+Farhan-Petkar@users.noreply.github.com>
2024-03-30 23:07:27 +0530

    Add files via upload

---
+++ Constantt.java
@@ -0,0 +1,13 @@
+package practiceofall;
+
+public class Constantt {
+    public static void main(String[] args) {
+        float pi = 3.14f;
+        System.out.println(pi);
+
+        // long l1 = 8591240491l;
+
+        char ch = (char)-300;
+        System.out.println(ch);
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,27 @@
+package practiceofall;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int i = 20;
+
+        System.out.println(i);
+
+        System.out.println(i++);
+
+        System.out.println(++i);
+
+        System.out.println(i++);
+
+        System.out.println(i);
+
+        System.out.println(i++);
+
+        System.out.println(++i);
+
+        System.out.println(i++);
+
+        System.out.println(++i);
+
+        System.out.println(i);
+    }
+}
